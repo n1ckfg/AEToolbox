80ec82786a2219a2b593a8127c30be9c8a5bc25d
Nick Fox-Gieg <nick@fox-gieg.com>
2016-07-18 15:22:27 -0400

    Vive quad splitter

--- AEToolbox.jsx
+++ AEToolbox.jsx
@@ -240,9 +240,94 @@
 
         if (theComp == null || !(theComp instanceof CompItem)){  
             alert(errorNoCompSelected);  
-        } else { 
-            var sideBySide = confirm("Use side-by-side stereo?");
+        } else {
+            if (theComp.selectedLayers.length == 0) {
+                alert(errorNoLayerSelected);
+            } else { 
+                baseWidth = theComp.width / 4;
+                baseHeight = theComp.height / 4
+                theComp.width = baseWidth * 2;
+                theComp.height = baseHeight * 2;
 
+                var titleBarSize = 36;
+                var scaleOffset = 0.5;
+                var theLayers = theComp.selectedLayers;
+
+                for (var i=0; i<theLayers.length; i++) {
+                    var curLayer = theLayers[i];                
+                    if (curLayer.matchName == "ADBE AV Layer") {
+                        var origName = curLayer.name;                        
+
+                        var precomp = theComp.layers.precompose([curLayer.index], "Precomp " + origName, true);
+                        precomp.width = baseWidth * 4;
+                        precomp.height = (baseHeight * 4) - titleBarSize;
+                        precomp.layers[1].transform.position.setValue([baseWidth * 2, (baseHeight * 2) - titleBarSize]);
+
+                        var precomp2 = theComp.layers.precompose([theComp.selectedLayers[0].index], "Precomp " + origName + " 2", true);
+                        precomp2.width = baseWidth * 4;
+                        precomp2.height = baseHeight * 4;
+                        precomp2.layers[1].transform.scale.setValue([100, 100 * (precomp2.height/precomp.height)]);
+                        precomp2.layers[1].transform.position.setValue([baseWidth * 2, baseHeight * 2]);
+
+                        var fgLayer = theComp.selectedLayers[0];
+                        fgLayer.name =  origName + "_foreground";
+                        fgLayer.transform.anchorPoint.setValue([baseWidth, baseHeight]);
+                        fgLayer.transform.position.setValue([baseWidth, baseHeight]);
+
+                        var alphaLayer = fgLayer.duplicate();
+                        alphaLayer.name =  origName + "_alpha";
+                        alphaLayer.transform.anchorPoint.setValue([baseWidth * 3, baseHeight]);
+                        alphaLayer.transform.position.setValue([baseWidth, baseHeight]);
+
+                        var cameraPovLayer = fgLayer.duplicate();
+                        cameraPovLayer.name = origName + "_camera_POV";
+                        cameraPovLayer.transform.anchorPoint.setValue([baseWidth, baseHeight * 3]);
+                        cameraPovLayer.transform.position.setValue([baseWidth, baseHeight]);
+
+                        var playerPovLayer = fgLayer.duplicate();
+                        playerPovLayer.name = origName + "_player_POV";
+                        playerPovLayer.transform.anchorPoint.setValue([baseWidth * 3, baseHeight * 3]);
+                        playerPovLayer.transform.position.setValue([baseWidth, baseHeight]);
+
+                        fgLayer.transform.scale.setValue([100, 100 + scaleOffset]);
+                        alphaLayer.transform.scale.setValue([100, 100 + scaleOffset]);
+                        cameraPovLayer.transform.scale.setValue([100, 100 + scaleOffset]);
+                        playerPovLayer.transform.scale.setValue([100, 100 + scaleOffset]);
+
+                        var fg_SavedIndex = fgLayer.index;
+                        var fgLayer2 = theComp.layers.precompose([fg_SavedIndex], fgLayer.name, true);
+                        fgLayer2.width = baseWidth * 2;
+                        fgLayer2.height = baseHeight * 2;
+                        fgLayer2.layers[1].collapseTransformation = true;
+                        theComp.layers[fg_SavedIndex].audioEnabled = false;
+
+                        var alpha_SavedIndex = alphaLayer.index;
+                        var alphaLayer2 = theComp.layers.precompose([alpha_SavedIndex], alphaLayer.name, true);
+                        alphaLayer2.width = baseWidth * 2;
+                        alphaLayer2.height = baseHeight * 2;
+                        alphaLayer2.layers[1].collapseTransformation = true;
+                        theComp.layers[alpha_SavedIndex].audioEnabled = false;
+
+                        var cameraPov_SavedIndex = cameraPovLayer.index;
+                        var cameraPovLayer2 = theComp.layers.precompose([cameraPov_SavedIndex], cameraPovLayer.name, true);
+                        cameraPovLayer2.width = baseWidth * 2;
+                        cameraPovLayer2.height = baseHeight * 2;
+                        cameraPovLayer2.layers[1].collapseTransformation = true;
+                        theComp.layers[cameraPov_SavedIndex].audioEnabled = true;
+
+                        var playerPov_SavedIndex = playerPovLayer.index;
+                        var playerPovLayer2 = theComp.layers.precompose([playerPov_SavedIndex], playerPovLayer.name, true);
+                        playerPovLayer2.width = baseWidth * 2;
+                        playerPovLayer2.height = baseHeight * 2;
+                        playerPovLayer2.layers[1].collapseTransformation = true;
+                        theComp.layers[playerPov_SavedIndex].audioEnabled = false;
+                    } else {
+                        alert(errorFootageOnly);
+                    }
+                }
+            //var sideBySide = confirm("Use side-by-side stereo?");
+
+            /*
             var newComp1 = theComp.duplicate();
             newComp1.name = theComp.name + " L";
             var theLayers1 = newComp1.layers;
@@ -287,6 +372,8 @@
             }
 
             theComp.name = "Precomp s3D Pair";
+            */
+            }
         }
 
         app.endUndoGroup();    
