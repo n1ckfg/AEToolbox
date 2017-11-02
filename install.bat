4ff725ace7a874217a6802ee68fac83afa942727
Nick Fox-Gieg <nick@fox-gieg.com>
2017-11-02 10:30:44 -0400

    install files

--- install.bat
+++ install.bat
@@ -1,6 +1,10 @@
 @echo off
 
 cd %cd%
+copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CS6\Support Files\Scripts\ScriptUI Panels"
+copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CC\Support Files\Scripts\ScriptUI Panels"
+copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CC 2014\Support Files\Scripts\ScriptUI Panels"
+copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CC 2015\Support Files\Scripts\ScriptUI Panels"
 copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CC 2015.3\Support Files\Scripts\ScriptUI Panels"
 copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CC 2017\Support Files\Scripts\ScriptUI Panels"
 copy AEToolbox.jsx "C:\Program Files\Adobe\Adobe After Effects CC 2018\Support Files\Scripts\ScriptUI Panels"
