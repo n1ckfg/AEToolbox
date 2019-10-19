24a1b25f8a79cb2d8e88156d342b2fd0c6e9abd7
Nick Fox-Gieg <nick@fox-gieg.com>
2019-10-19 11:46:39 -0400

    add image category

--- build/aetoolbox_main.js
+++ build/aetoolbox_main.js
@@ -38,8 +38,8 @@ function init(_panel) {
         var colXinc = 170;
 
         // Basic group
-        var col1butCount = 8;
-        panel.basicGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col1butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+        var col0butCount = 8;
+        panel.basicGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col0butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
         panel.basicGroup0 = panel.basicGroup.add("button", [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], "Nulls for Pins");
         panel.basicGroup1 = panel.basicGroup.add("button", [butXstart,butYstart+(butYinc*1),butXend,butYend+(butYinc*1)], "Parent Chain");
         panel.basicGroup2 = panel.basicGroup.add("button", [butXstart,butYstart+(butYinc*2),butXend,butYend+(butYinc*2)], "Locator Null");
@@ -50,8 +50,8 @@ function init(_panel) {
         panel.basicGroup7 = panel.basicGroup.add("button", [butXstart,butYstart+(butYinc*7),butXend,butYend+(butYinc*7)], "Isolate Color");
        
         // Advanced group
-        var col2butCount = 6;
-        panel.advGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col2butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+        var col1butCount = 6;
+        panel.advGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col1butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
         panel.advGroup0 = panel.advGroup.add("button", [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], "Bake Keyframes");
         panel.advGroup1 = panel.advGroup.add("button", [butXstart,butYstart+(butYinc*1),butXend,butYend+(butYinc*1)], "Lock Y Rotation");
         panel.advGroup2 = panel.advGroup.add("button", [butXstart,butYstart+(butYinc*2),butXend,butYend+(butYinc*2)], "Auto Z Rotation");
@@ -60,8 +60,8 @@ function init(_panel) {
         panel.advGroup5 = panel.advGroup.add("button", [butXstart,butYstart+(butYinc*5),butXend,butYend+(butYinc*5)], "Crossfade");
         
         // Rigging group
-        var col3butCount = 7;
-        panel.rigGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col3butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+        var col2butCount = 7;
+        panel.rigGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col2butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
         panel.rigGroup0 = panel.rigGroup.add("button", [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], "Blink Rig");
         panel.rigGroup1 = panel.rigGroup.add("button", [butXstart,butYstart+(butYinc*1),butXend,butYend+(butYinc*1)], "Jaw Rig");
         panel.rigGroup2 = panel.rigGroup.add("button", [butXstart,butYstart+(butYinc*2),butXend,butYend+(butYinc*2)], "Snake Rig");
@@ -71,8 +71,8 @@ function init(_panel) {
         panel.rigGroup6 = panel.rigGroup.add("button", [butXstart,butYstart+(butYinc*6),butXend,butYend+(butYinc*6)], "Photo Rig");
       
         // Depth group
-        var col4butCount = 7;
-        panel.depthGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col4butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+        var col3butCount = 7;
+        panel.depthGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col3butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
         panel.depthGroup0 = panel.depthGroup.add("button", [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], "Split s3D Pair");
         panel.depthGroup1 = panel.depthGroup.add("button", [butXstart,butYstart+(butYinc*1),butXend,butYend+(butYinc*1)], "Merge s3D Pair");
         panel.depthGroup2 = panel.depthGroup.add("button", [butXstart,butYstart+(butYinc*2),butXend,butYend+(butYinc*2)], "s3D Dispmap");
@@ -82,13 +82,18 @@ function init(_panel) {
         panel.depthGroup6 = panel.depthGroup.add("button", [butXstart,butYstart+(butYinc*6),butXend,butYend+(butYinc*6)], "Gray to RGB");
       
         // Picture-in-picture / Reformatting group
-        var col5butCount = 5;
-        panel.pipGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col5butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+        var col4butCount = 5;
+        panel.pipGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col4butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
         panel.pipGroup0 = panel.pipGroup.add("button", [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], "Vive Recording");
         panel.pipGroup1 = panel.pipGroup.add("button", [butXstart,butYstart+(butYinc*1),butXend,butYend+(butYinc*1)], "Holoflix");
         panel.pipGroup2 = panel.pipGroup.add("button", [butXstart,butYstart+(butYinc*2),butXend,butYend+(butYinc*2)], "RGBD Toolkit");
         panel.pipGroup3 = panel.pipGroup.add("button", [butXstart,butYstart+(butYinc*3),butXend,butYend+(butYinc*3)], "InstaGrid");
         panel.pipGroup4 = panel.pipGroup.add("button", [butXstart,butYstart+(butYinc*4),butXend,butYend+(butYinc*4)], "4K Stereo 360");
+
+        // Image Effect group
+        var col5butCount = 1;
+        panel.imageGroup = panel.add("panel", [colXstart, colYstart, colXend, colYendBase+(col5butCount*butYinc)+butYoffset+butYoffsetCap], "", {borderStyle: "etched"});
+        panel.imageGroup0 = panel.imageGroup.add("button", [butXstart,butYstart+(butYinc*0),butXend,butYend+(butYinc*0)], "Soften");
 
         // Guide group
         var col6butCount = 2;
@@ -150,6 +155,8 @@ function init(_panel) {
         panel.depthGroup5.onClick = stereoController;
         panel.depthGroup6.onClick = doRgbToGray;
         //--
+        panel.imageGroup0.onclick = splitStereoPair;
+        //--
         panel.pipGroup0.onClick = viveRecording;
         panel.pipGroup1.onClick = holoflix720p;
         panel.pipGroup2.onClick = rgbdtk;
@@ -209,6 +216,8 @@ function init(_panel) {
         panel.pipGroup3.helpTip = "Turns six Instagram clips into a 3 x 2 HD grid." //instaGrid;
         panel.pipGroup4.helpTip = "Creates a 4K OU 360 stereo comp." //stereo360;
         //--
+        panel.imageGroup0.helpTip = "Creates an adjustment layer that applies an onion skin effect."; //onionSkin;
+        //--
         panel.guideGroup0.helpTip = "Creates an adjustment layer that applies an onion skin effect."; //onionSkin;
         panel.guideGroup1.helpTip = "View connections between parent and child layers."; //skeleView;
         //--
@@ -225,7 +234,7 @@ function init(_panel) {
 
         // 4-5. Selector
         //-----------------------------------------------------
-        var selector = panel.add("dropdownlist",[colXstart, colYstart, colXend, colYendBase],[ "Basic", "Advanced", "Rigging", "Depth", "Reformat", "Guide", "Export", "Import", "Plugins" ]);
+        var selector = panel.add("dropdownlist",[colXstart, colYstart, colXend, colYendBase],[ "Basic", "Advanced", "Rigging", "Depth", "Reformat", "Image", "Guide", "Export", "Import", "Plugins" ]);
 
         selector.onChange = function() {
             panel.basicGroup.visible = false;
@@ -233,6 +242,7 @@ function init(_panel) {
             panel.rigGroup.visible = false;
             panel.depthGroup.visible = false;
             panel.pipGroup.visible = false;
+            panel.imageGroup.visible = false;
             panel.guideGroup.visible = false;
             panel.exportGroup.visible = false;    
             panel.importGroup.visible = false;    
@@ -248,13 +258,15 @@ function init(_panel) {
                 panel.depthGroup.visible = true;
             } else if (selector.selection == 4) { // PiP
                 panel.pipGroup.visible = true;
-            } else if (selector.selection == 5) { // Guide
+            } else if (selector.selection == 5) { // Image
+                panel.imageGroup.visible = true;
+            } else if (selector.selection == 6) { // Guide
                 panel.guideGroup.visible = true;
-            } else if (selector.selection == 6) { // Export
+            } else if (selector.selection == 7) { // Export
                 panel.exportGroup.visible = true;
-            } else if (selector.selection == 7) { // Import
+            } else if (selector.selection == 8) { // Import
                 panel.importGroup.visible = true;
-            } else if (selector.selection == 8) { // Plugins
+            } else if (selector.selection == 9) { // Plugins
                 panel.pluginGroup.visible = true;
             }      
         }
