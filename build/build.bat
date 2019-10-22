0c51f3de472a8aa0493e71a20f37fa66e41387c1
Nick Fox-Gieg <nick@fox-gieg.com>
2019-10-22 10:19:04 -0400

    shell scripts

--- build/build.bat
+++ build/build.bat
@@ -8,13 +8,14 @@ del %BUILD_TARGET%
 
 copy /b aetoolbox_main.js+aetoolbox_basic.js+aetoolbox_advanced.js+aetoolbox_rigging.js+aetoolbox_stereo.js+aetoolbox_pip.js+aetoolbox_image.js+aetoolbox_guide.js+aetoolbox_export.js+aetoolbox_import.js+aetoolbox_plugins.js+aetoolbox_util.js %BUILD_TARGET%
 
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CS6\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2014\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2015\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2015.3\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2017\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2018\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CS6\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2014\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2015\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2015.3\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2017\Support Files\Scripts\ScriptUI Panels\"
+rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2018\Support Files\Scripts\ScriptUI Panels\"
 copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2019\Support Files\Scripts\ScriptUI Panels\"
+copy %BUILD_TARGET% "D:\Adobe\Adobe After Effects CC 2019\Support Files\Scripts\ScriptUI Panels\"
 
 @pause
--- build/build.command
+++ build/build.command
@@ -17,11 +17,11 @@ touch $BUILD_TARGET
 
 cat "aetoolbox_main.js" "aetoolbox_basic.js" "aetoolbox_advanced.js" "aetoolbox_rigging.js" "aetoolbox_stereo.js" "aetoolbox_pip.js" "aetoolbox_image.js" "aetoolbox_guide.js" "aetoolbox_export.js" "aetoolbox_import.js" "aetoolbox_plugins.js" "aetoolbox_util.js" > $BUILD_TARGET
 
-cp $BUILD_TARGET "/Applications/Adobe After Effects CS6/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2014/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2015.3/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2017/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CS6/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CC/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2014/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2015.3/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2017/Scripts/ScriptUI Panels/"
+#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/"
 cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2019/Scripts/ScriptUI Panels/"
