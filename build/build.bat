46b7f69cc339a03822920c148b3dd913ccb996eb
Nick Fox-Gieg <nick@fox-gieg.com>
2023-05-19 17:08:30 -0400

    shell

--- build/build.bat
+++ build/build.bat
@@ -1,23 +1,29 @@
 @echo off
 
-cd %~dp0
+cd /d %~dp0
 
-set BUILD_TARGET=..\AEToolbox.jsx
+set BUILD_TARGET=%~dp0..\AEToolbox.jsx
 
 del %BUILD_TARGET%
 
 copy /b aetoolbox_main.js+aetoolbox_basic.js+aetoolbox_advanced.js+aetoolbox_rigging.js+aetoolbox_stereo.js+aetoolbox_pip.js+aetoolbox_image.js+aetoolbox_guide.js+aetoolbox_export.js+aetoolbox_import.js+aetoolbox_plugins.js+aetoolbox_util.js %BUILD_TARGET%
 
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CS6\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2014\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2015\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2015.3\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2017\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2018\Support Files\Scripts\ScriptUI Panels\"
-rem copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2019\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects CC 2020\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects 2021\Support Files\Scripts\ScriptUI Panels\"
-copy %BUILD_TARGET% "C:\Program Files\Adobe\Adobe After Effects 2022\Support Files\Scripts\ScriptUI Panels\"
+set VERSION=Adobe After Effects 2023
+
+set DEST="C:\Program Files\Adobe\%VERSION%\Support Files\Scripts\ScriptUI Panels\"
+
+copy %BUILD_TARGET% %DEST%
 
 @pause
+
+rem set VERSION="Adobe After Effects CS6"
+rem set VERSION="Adobe After Effects CC"
+rem set VERSION="Adobe After Effects CC 2014"
+rem set VERSION="Adobe After Effects CC 2015"
+rem set VERSION="Adobe After Effects CC 2015.3"
+rem set VERSION="Adobe After Effects CC 2017"
+rem set VERSION="Adobe After Effects CC 2018"
+rem set VERSION="Adobe After Effects CC 2019"
+rem set VERSION="Adobe After Effects CC 2020"
+rem set VERSION="Adobe After Effects 2021"
+rem set VERSION="Adobe After Effects 2022"
--- build/build.command
+++ build/build.command
@@ -17,14 +17,20 @@ touch $BUILD_TARGET
 
 cat "aetoolbox_main.js" "aetoolbox_basic.js" "aetoolbox_advanced.js" "aetoolbox_rigging.js" "aetoolbox_stereo.js" "aetoolbox_pip.js" "aetoolbox_image.js" "aetoolbox_guide.js" "aetoolbox_export.js" "aetoolbox_import.js" "aetoolbox_plugins.js" "aetoolbox_util.js" > $BUILD_TARGET
 
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CS6/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2014/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2015/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2015.3/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2017/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2018/Scripts/ScriptUI Panels/"
-#cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2019/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects CC 2020/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects 2021/Scripts/ScriptUI Panels/"
-cp $BUILD_TARGET "/Applications/Adobe After Effects 2022/Scripts/ScriptUI Panels/"
+VERSION="Adobe After Effects 2023"
+
+DEST="/Applications/$VERSION/Scripts/ScriptUI Panels/"
+
+cp $BUILD_TARGET $DEST
+
+#VERSION="Adobe After Effects CS6"
+#VERSION="Adobe After Effects CC"
+#VERSION="Adobe After Effects CC 2014"
+#VERSION="Adobe After Effects CC 2015"
+#VERSION="Adobe After Effects CC 2015.3"
+#VERSION="Adobe After Effects CC 2017"
+#VERSION="Adobe After Effects CC 2018"
+#VERSION="Adobe After Effects CC 2019"
+#VERSION="Adobe After Effects CC 2020"
+#VERSION="Adobe After Effects 2021"
+#VERSION="Adobe After Effects 2022"
